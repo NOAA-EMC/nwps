10c76fc23dc8012394281c42c1e728a685e15185
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2019-04-11 19:12:57 +0000

    Included clustering-based wave tracking

--- ush/nwps_wavetrack_cg1.sh
+++ ush/nwps_wavetrack_cg1.sh
@@ -29,6 +29,31 @@ if [ "${hastracking}" == "TRUE" ]
    echo "perl -I${PMnwps} -I${RUNdir} ${USHnwps}/waveTracking.pl" | tee -a $logfile    
    perl -I${PMnwps} -I${RUNdir} ${USHnwps}/waveTracking.pl | tee -a $logfile
    export err=$?; err_chk
+   #AW073017 ----- Convert partition file to Python scikit learn format ----
+   ${EXECnwps}/ww3_sysprep.exe
+   #AW073017 ---------------------------------------------------------------
+   #AW020218 ----- Run Python scikit learn version of wave tracking --------
+   mv -fv ${OUTPUTdir}/partition/CG1/SYS_HSIGN.OUT ${OUTPUTdir}/partition/CG1/SYS_HSIGN.OUT.SPRL
+   mv -fv ${OUTPUTdir}/partition/CG1/SYS_TP.OUT ${OUTPUTdir}/partition/CG1/SYS_TP.OUT.SPRL
+   mv -fv ${OUTPUTdir}/partition/CG1/SYS_DIR.OUT ${OUTPUTdir}/partition/CG1/SYS_DIR.OUT.SPRL
+   mv -fv ${OUTPUTdir}/partition/CG1/SYS_DSPR.OUT ${OUTPUTdir}/partition/CG1/SYS_DSPR.OUT.SPRL
+   mv -fv ${OUTPUTdir}/partition/CG1/SYS_PNT.OUT ${OUTPUTdir}/partition/CG1/SYS_PNT.OUT.SPRL
+
+   cd ${RUNdir}
+   cp ${NWPSdir}/sorc/ww3_syscluster/coastal_bound_high.txt .
+
+   #aprun -n1 -N1 -d1 ${PYTHON} ${NWPSdir}/sorc/ww3_syscluster/ww3_systrk_nobasemap_tables_gh.py ${SITEID,,}
+   perl -I${PMnwps} -I${RUNdir} ${USHnwps}/waveTracking_clust.pl | tee -a $logfile
+
+   #${USHnwps}/postprocess_partition_fields_clust.sh ${SITEID} CG1
+
+   cycle=$(awk '{print $1;}' ${RUNdir}/CYCLE)
+   COMOUTCYC="${COMOUT}/${cycle}/CG0"
+   tar -czvf cluster_plots_CG0_${PDY}${cycle}.tar.gz swan_systrk1_hr???.png
+   cp -fv ${RUNdir}/cluster_plots_CG0_${PDY}${cycle}.tar.gz $COMOUTCYC/
+   rm ${RUNdir}/swan_systrk1_hr???.png
+   rm ${RUNdir}/coastal_bound_high.txt
+   #AW020218 ---------------------------------------------------------------
 fi
 
 #########################################################################
