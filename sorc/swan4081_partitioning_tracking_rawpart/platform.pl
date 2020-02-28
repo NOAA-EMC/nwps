57b8e1fc4110231c2a7810db25149709ec12b705
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-02-28 14:50:15 +0000

    Removed -ffree-line-length-none compiler option to remove compilation warning

--- sorc/swan4081_partitioning_tracking_rawpart/platform.pl
+++ sorc/swan4081_partitioning_tracking_rawpart/platform.pl
@@ -163,7 +163,7 @@ elsif ($os =~ /Linux/i) {
     #print OUTFILE "FLAGS_OPT = \n";
     print OUTFILE "FLAGS_OPT = -c\n";
     print OUTFILE "FLAGS_MSC = -g -O2 \n";
-    print OUTFILE "FLAGS90_MSC = \$(FLAGS_MSC) -ffree-line-length-none\n";
+    print OUTFILE "FLAGS90_MSC = \$(FLAGS_MSC)\n";
 
 #    print OUTFILE "FLAGS_OPT = -g -O2 -v \n";
 #    print OUTFILE "FLAGS_MSC =\n";
