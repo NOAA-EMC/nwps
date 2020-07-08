79ecb9614216a41f40d7dd8fdb934a0b7e74d126
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-07-08 14:04:24 +0000

    Renamed from make_rip_current_program.sh to make_ripforecast.sh to match executable name

---
+++ sorc/make_ripforecast.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+export pwd=`pwd`
+export NWPSdir=${pwd%/*}
+
+if [ "${NWPSdir}" == "" ]
+    then 
+    echo "ERROR - Your NWPSdir variable is not set"
+    exit 1
+fi
+
+## Setup our build environment
+#source ${NWPSdir}/sorc/set_compiler.sh
+
+#module purge
+#module load ncep
+#module load ../modulefiles/NWPS/v1.3.0
+#module list
+
+cd ${NWPSdir}/sorc/ripforecast.fd/
+make ripforecast | tee ./ripcurrent_build.log
+rm *.o
+mv -v ripforecast.exe ${NWPSdir}/exec/ripforecast.exe
+
+echo "Build complete"
+exit 0
