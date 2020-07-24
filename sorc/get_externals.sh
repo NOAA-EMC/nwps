2a19257a9fcd69c4e0eed0ef076225118fcf633e
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-07-24 11:21:49 +0000

    Moved external fix data files and binaries to polar ftp

--- sorc/get_externals.sh
+++ sorc/get_externals.sh
@@ -13,14 +13,14 @@ if [ "${NWPSdir}" == "" ]
 fi
 
 echo 'Fetching externals...'
-scp andre.westhuysen@emcrzdm:/home/www/polar/nwps/EMC_nwps_external/fix/bathy_db.tar ${NWPSdir}/fix/
+wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/fix/bathy_db.tar -P ${NWPSdir}/fix/
 tar -C ${NWPSdir}/fix/ -xvf ${NWPSdir}/fix/bathy_db.tar
 rm ${NWPSdir}/fix/bathy_db.tar
-scp andre.westhuysen@emcrzdm:/home/www/polar/nwps/EMC_nwps_external/fix/pdef_ncep_global ${NWPSdir}/fix/
-scp andre.westhuysen@emcrzdm:/home/www/polar/nwps/EMC_nwps_external/ush/rtofs/datfiles/pdef_ncep_global.gz ${NWPSdir}/ush/rtofs/datfiles/
-scp andre.westhuysen@emcrzdm:/home/www/polar/nwps/EMC_nwps_external/ush/rtofs/datfiles/pdef_ncep_reg1.gz ${NWPSdir}/ush/rtofs/datfiles/
-scp andre.westhuysen@emcrzdm:/home/www/polar/nwps/EMC_nwps_external/ush/rtofs/datfiles/pdef_ncep_reg2.gz ${NWPSdir}/ush/rtofs/datfiles/
-scp andre.westhuysen@emcrzdm:/home/www/polar/nwps/EMC_nwps_external/ush/rtofs/datfiles/pdef_ncep_reg3.gz ${NWPSdir}/ush/rtofs/datfiles/
-scp andre.westhuysen@emcrzdm:/home/www/polar/nwps/EMC_nwps_external/ush/python/etc/default/rdat.tar ${NWPSdir}/ush/python/etc/default
+wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/fix/pdef_ncep_global -P ${NWPSdir}/fix/
+wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/ush/rtofs/datfiles/pdef_ncep_global.gz -P ${NWPSdir}/ush/rtofs/datfiles/
+wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/ush/rtofs/datfiles/pdef_ncep_reg1.gz -P ${NWPSdir}/ush/rtofs/datfiles/
+wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/ush/rtofs/datfiles/pdef_ncep_reg2.gz -P ${NWPSdir}/ush/rtofs/datfiles/
+wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/ush/rtofs/datfiles/pdef_ncep_reg3.gz -P ${NWPSdir}/ush/rtofs/datfiles/
+wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/ush/python/etc/default/rdat.tar -P ${NWPSdir}/ush/python/etc/default/
 tar -C ${NWPSdir}/ush/python/etc/default/ -xvf ${NWPSdir}/ush/python/etc/default/rdat.tar
 rm ${NWPSdir}/ush/python/etc/default/rdat.tar
