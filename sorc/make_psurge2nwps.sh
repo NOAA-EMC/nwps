1fc2faec22aa28a57370fffbc0b9f1de56648405
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-08-03 14:54:44 +0000

    Included build for psurge_combine.exe

--- sorc/make_psurge2nwps.sh
+++ sorc/make_psurge2nwps.sh
@@ -20,7 +20,7 @@ make | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psoutTOnwps_build.log
 
 cd ${NWPSdir}/sorc/psurge2nwps.cd
 #ifort -O2 -g -traceback -v -o -o psoutTOnwps.exe psoutTOnwps_ver04.f
-ftn -o psoutTOnwps.exe psoutTOnwps.f | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psoutTOnwps_build.log
+ftn -o psoutTOnwps.exe psoutTOnwps.f | tee ${NWPSdir}/sorc/psurge2nwps.cd/psoutTOnwps_build.log
 mv -v psoutTOnwps.exe ${NWPSdir}/exec/psoutTOnwps.exe
 echo "Build complete" | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psoutTOnwps_build.log
 
@@ -28,6 +28,20 @@ echo "Building psurge_identify"  | tee ${NWPSdir}/sorc/psurge2nwps.cd/psurge_ide
 ftn -o psurge_identify.exe psurge_identify.f | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psurge_identify_build.log
 mv -v psurge_identify.exe ${NWPSdir}/exec/psurge_identify.exe
 echo "Build complete" | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psurge_identify_build.log
+
+echo "Building psurge_combine" | tee ${NWPSdir}/sorc/psurge2nwps.cd/psurge_combine_build.log
+rm ${NWPSdir}/lib/r8lib.o | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psurge_combine_build.log
+cd ${NWPSdir}/lib/sorc/r8lib/
+ftn -c r8lib.f90 | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psurge_combine_build.log
+mv -v r8lib.o ${NWPSdir}/lib/r8lib.o
+
+cd ${NWPSdir}/sorc/psurge2nwps.cd/
+ftn -c pwl_interp_2d.f90 | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psurge_combine_build.log
+ftn -c psurge_combine.f90 | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psurge_combine_build.log
+ftn -o psurge_combine.exe psurge_combine.o pwl_interp_2d.o ${NWPSdir}/lib/r8lib.o | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psurge_combine_build.log
+rm psurge_combine.o pwl_interp_2d.o | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psurge_combine_build.log
+mv -v psurge_combine.exe ${NWPSdir}/exec/psurge_combine.exe
+echo "Build complete" | tee -a ${NWPSdir}/sorc/psurge2nwps.cd/psurge_combine_build.log
 
 echo "Building psurge2nwps"  | tee ${NWPSdir}/sorc/psurge2nwps.cd/psurge2nwps_build.log
 cd ${NWPSdir}/sorc/psurge2nwps.cd
