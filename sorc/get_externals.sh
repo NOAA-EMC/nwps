cfa35159f9f76f811fdbb6cdafffcd5c3416b4c5
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-08-07 19:16:16 +0000

    Changed location of external fix files from ftp polar to a wavepa directory on WCOSS

--- sorc/get_externals.sh
+++ sorc/get_externals.sh
@@ -13,14 +13,14 @@ if [ "${NWPSdir}" == "" ]
 fi
 
 echo 'Fetching externals...'
-wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/fix/bathy_db.tar -P ${NWPSdir}/fix/
+scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/fix/bathy_db.tar ${NWPSdir}/fix/
 tar -C ${NWPSdir}/fix/ -xvf ${NWPSdir}/fix/bathy_db.tar
 rm ${NWPSdir}/fix/bathy_db.tar
-wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/fix/pdef_ncep_global -P ${NWPSdir}/fix/
-wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/ush/rtofs/datfiles/pdef_ncep_global.gz -P ${NWPSdir}/ush/rtofs/datfiles/
-wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/ush/rtofs/datfiles/pdef_ncep_reg1.gz -P ${NWPSdir}/ush/rtofs/datfiles/
-wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/ush/rtofs/datfiles/pdef_ncep_reg2.gz -P ${NWPSdir}/ush/rtofs/datfiles/
-wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/ush/rtofs/datfiles/pdef_ncep_reg3.gz -P ${NWPSdir}/ush/rtofs/datfiles/
-wget ftp://polar.ncep.noaa.gov/tempor/nwps_v1-3_ftp/ush/python/etc/default/rdat.tar -P ${NWPSdir}/ush/python/etc/default/
+scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/fix/pdef_ncep_global ${NWPSdir}/fix/
+scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/ush/rtofs/datfiles/pdef_ncep_global.gz ${NWPSdir}/ush/rtofs/datfiles/
+scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/ush/rtofs/datfiles/pdef_ncep_reg1.gz ${NWPSdir}/ush/rtofs/datfiles/
+scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/ush/rtofs/datfiles/pdef_ncep_reg2.gz ${NWPSdir}/ush/rtofs/datfiles/
+scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/ush/rtofs/datfiles/pdef_ncep_reg3.gz ${NWPSdir}/ush/rtofs/datfiles/
+scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/ush/python/etc/default/rdat.tar ${NWPSdir}/ush/python/etc/default
 tar -C ${NWPSdir}/ush/python/etc/default/ -xvf ${NWPSdir}/ush/python/etc/default/rdat.tar
 rm ${NWPSdir}/ush/python/etc/default/rdat.tar
