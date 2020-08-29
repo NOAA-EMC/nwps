17f3d4adf9e2491024b1bc01584c2816effb02aa
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-08-29 14:05:20 +0000

    Set Cartopy to use local shapefiles in lib/cartopy

--- ush/python/owash.py
+++ ush/python/owash.py
@@ -2,6 +2,7 @@
 # Author: Andre van der Westhuysen, 12/15/16
 # Purpose: Plots SWAN output parameters from GRIB2.
 
+import cartopy
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib
@@ -12,8 +13,14 @@ import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
-#from mpl_toolkits.basemap import Basemap
 from matplotlib import colors
+
+print('*** owash.py ***')
+
+NWPSdir = os.environ['NWPSdir']
+cartopy.config['pre_existing_data_dir'] = NWPSdir+'/lib/cartopy'
+print('Reading cartopy shapefiles from:')
+print(cartopy.config['pre_existing_data_dir'])
 
 # Parameters
 monthstr = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
@@ -24,7 +31,6 @@ nws_logo = plt.imread('NWS_Logo.png')
 usgs_logo = plt.imread('USGS_Logo.png')
 
 # Read control file
-print('*** owash.py ***')
 if os.path.isfile("swan_riprunup.ctl"):
    print('Reading: swan_riprunup.ctl')
 
@@ -210,7 +216,7 @@ for tstep in range(1, (int(TDEF)+1)):
       plt.plot(npierlons, npierlats, color="black", linewidth=2.5, linestyle="-", transform=ccrs.PlateCarree())
       plt.plot(spierlons, spierlats, color="black", linewidth=2.5, linestyle="-", transform=ccrs.PlateCarree())
 
-   figtitle = '** EXPERIMENTAL **   NWPS-USGS Overwash Probability (%) \n Hour '\
+   figtitle = 'NWPS-USGS Overwash Probability (%) \n Hour '\
               +str(forecastTime)+' ('+str(date.hour).zfill(2)+'Z'+str(date.day).zfill(2)\
               +monthstr[int(date.month)-1]+str(date.year)+')'
    plt.title(figtitle,fontsize=10)
