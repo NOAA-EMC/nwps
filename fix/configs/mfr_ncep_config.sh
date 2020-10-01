dd6e28c04572a4dfb385ed5cd8cf4928d3cefa73
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-10-01 19:44:38 +0000

    Update for Global ESTOFS

--- fix/configs/mfr_ncep_config.sh
+++ fix/configs/mfr_ncep_config.sh
@@ -1,15 +1,15 @@
 # NCEP Config file for global RTOFS and ESTOF init files
 
 # RTOFS Domain for ocean currents
-export RTOFSSECTOR="west_conus"
+export RTOFSSECTOR="west_conus.west"
 # RTOFSDOMAIN="LON LAT 0. NX NY EW-RESOLUTION NS-RESOLUTION"
 export RTOFSDOMAIN="232.20 40.50 0.0  159 165 0.029326 0.027027"
 export RTOFSNX="160"
 export RTOFSNY="166"
 
 # ESTOFS Domain for water level
-export ESTOFS_BASIN="estofs.pac"
-export ESTOFS_REGION="conus"
+export ESTOFS_BASIN="estofs.glo"
+export ESTOFS_REGION="conus.west"
 # ESFOTSDOMAIN="LON LAT 0. NX NY EW-RESOLUTION NS-RESOLUTION"
 export ESTOFSDOMAIN="232.20 40.50 0.0  159 165 0.029326 0.027027"
 export ESTOFSNX="160"
