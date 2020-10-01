2e00e33c3f17bf7a667f5d347f66d99b585a392f
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-10-01 19:35:20 +0000

    Update for Global ESTOFS

--- fix/configs/hgx_ncep_config.sh
+++ fix/configs/hgx_ncep_config.sh
@@ -8,8 +8,8 @@ export RTOFSNX="173"
 export RTOFSNY="224"
 
 # ESTOFS Domain for water level
-export ESTOFS_BASIN="estofs.atl"
-export ESTOFS_REGION="conus"
+export ESTOFS_BASIN="estofs.glo"
+export ESTOFS_REGION="conus.east"
 # ESFOTSDOMAIN="LON LAT 0. NX NY EW-RESOLUTION NS-RESOLUTION"
 export ESTOFSDOMAIN="262.10 26.50 0. 172  223 0.029326 0.027027"
 export ESTOFSNX="173"
