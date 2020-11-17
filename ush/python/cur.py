014d1036580a8702fff889da90da28bbf14fae21
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-11-17 18:39:23 +0000

    Implemented user-defined TSTART and TEND for plotting

--- ush/python/cur.py
+++ ush/python/cur.py
@@ -16,6 +16,10 @@ from matplotlib.colors import LinearSegmentedColormap
 from matplotlib.colors import Normalize
 
 print('*** cur.py ***')
+TSTART = int(sys.argv[1])
+TEND = int(sys.argv[2])
+print('TSTART = '+str(TSTART))
+print('TEND = '+str(TEND))
 
 NWPSdir = os.environ['NWPSdir']
 cartopy.config['pre_existing_data_dir'] = NWPSdir+'/lib/cartopy'
@@ -74,21 +78,21 @@ else:
    sys.exit()
 
 # Extract GRIB2 files to text
-for tstep in range(1, (int(TDEF)+1)):
+for tstep in range(TSTART, (int(TEND)+1)):
    print('')
    print('Extracting Time step: '+str(tstep))
 
    # Current speed
    grib2dump = 'SPC_extract_f'+str((tstep-1)*TINCR).zfill(3)+'.txt'
-   fieldmax = 'SPC_extract_fieldmax.txt'
+   #fieldmax = 'SPC_extract_fieldmax.txt'
    if tstep == 1:
       command = '$WGRIB2 '+DSET+' -s | grep "SPC:surface:anl" | $WGRIB2 -i '+DSET+' -spread '+grib2dump
-      command2 = '$WGRIB2 '+DSET+' -s | grep "SPC:surface:anl" | $WGRIB2 -i '+DSET+' -max | cat > '+fieldmax
+      #command2 = '$WGRIB2 '+DSET+' -s | grep "SPC:surface:anl" | $WGRIB2 -i '+DSET+' -max | cat > '+fieldmax
    else:
       command = '$WGRIB2 '+DSET+' -s | grep "SPC:surface:'+str((tstep-1)*TINCR)+' hour" | $WGRIB2 -i '+DSET+' -spread '+grib2dump
-      command2 = '$WGRIB2 '+DSET+' -s | grep "SPC:surface:'+str((tstep-1)*TINCR)+' hour" | $WGRIB2 -i '+DSET+' -max | cat >> '+fieldmax
+      #command2 = '$WGRIB2 '+DSET+' -s | grep "SPC:surface:'+str((tstep-1)*TINCR)+' hour" | $WGRIB2 -i '+DSET+' -max | cat >> '+fieldmax
    os.system(command)
-   os.system(command2)
+   #os.system(command2)
 
    # Primary wave direction
    grib2dump = 'DIRC_extract_f'+str((tstep-1)*TINCR).zfill(3)+'.txt'
@@ -114,6 +118,9 @@ if (lats.max()-lats.min()) > 0.5:
 else:
    dlat = (lats.max()-lats.min())/5.
 
+fieldmax = 'SPC_extract_fieldmax_TSTART'+str(TSTART)+'.txt'
+command = '$WGRIB2 '+DSET+' -s | grep "SPC" | $WGRIB2 -i '+DSET+' -max | cat > '+fieldmax
+os.system(command)
 SITEID = os.environ.get('SITEID')
 CGNUMPLOT = os.environ.get('CGNUMPLOT')
 
@@ -122,7 +129,7 @@ maxval=max(temp)
 
 plt.figure()
 # Read the extracted text file
-for tstep in range(1, (int(TDEF)+1)):
+for tstep in range(TSTART, (int(TEND)+1)):
    print('')
    print('Processing Time step: '+str(tstep))
 
@@ -276,6 +283,7 @@ for tstep in range(1, (int(TDEF)+1)):
    plt.clf()
 
 # Clean up text dump files
-os.system('rm SPC_extract*f???.txt')
-os.system('rm DIRC_extract*f???.txt')
+for tstep in range(TSTART, (int(TEND)+1)):
+   os.system('rm SPC_extract_f'+str((tstep-1)*TINCR).zfill(3)+'.txt')
+   os.system('rm DIRC_extract_f'+str((tstep-1)*TINCR).zfill(3)+'.txt')
 
