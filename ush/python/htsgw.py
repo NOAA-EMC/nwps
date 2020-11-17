b3ff947201f25b1912a5d8daa1274267314e1e1d
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-11-17 18:38:27 +0000

    Implemented user-defined TSTART and TEND for plotting

--- ush/python/htsgw.py
+++ ush/python/htsgw.py
@@ -16,6 +16,10 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
 print('*** htsgw.py ***')
+TSTART = int(sys.argv[1])
+TEND = int(sys.argv[2])
+print('TSTART = '+str(TSTART))
+print('TEND = '+str(TEND))
 
 NWPSdir = os.environ['NWPSdir']
 cartopy.config['pre_existing_data_dir'] = NWPSdir+'/lib/cartopy'
@@ -74,21 +78,20 @@ else:
    sys.exit()
 
 # Extract GRIB2 files to text
-for tstep in range(1, (int(TDEF)+1)):
+for tstep in range(TSTART, (int(TEND)+1)):
    print('')
    print('Extracting Time step: '+str(tstep))
 
    # Significant height of combined wind waves and swell
    grib2dump = 'HTSGW_extract_f'+str((tstep-1)*TINCR).zfill(3)+'.txt'
-   fieldmax = 'HTSGW_extract_fieldmax.txt'
    if tstep == 1:
       command = '$WGRIB2 '+DSET+' -s | grep "HTSGW:surface:anl" | $WGRIB2 -i '+DSET+' -rpn "sto_1:-9999:rcl_1:merge" -spread '+grib2dump
-      command2 = '$WGRIB2 '+DSET+' -s | grep "HTSGW:surface:anl" | $WGRIB2 -i '+DSET+' -max | cat > '+fieldmax
+      #command2 = '$WGRIB2 '+DSET+' -s | grep "HTSGW:surface:anl" | $WGRIB2 -i '+DSET+' -max | cat > '+fieldmax
    else:
       command = '$WGRIB2 '+DSET+' -s | grep "HTSGW:surface:'+str((tstep-1)*TINCR)+' hour" | $WGRIB2 -i '+DSET+' -rpn "sto_1:-9999:rcl_1:merge" -spread '+grib2dump
-      command2 = '$WGRIB2 '+DSET+' -s | grep "HTSGW:surface:'+str((tstep-1)*TINCR)+' hour" | $WGRIB2 -i '+DSET+' -max | cat >> '+fieldmax
+      #command2 = '$WGRIB2 '+DSET+' -s | grep "HTSGW:surface:'+str((tstep-1)*TINCR)+' hour" | $WGRIB2 -i '+DSET+' -max | cat >> '+fieldmax
    os.system(command)
-   os.system(command2)
+   #os.system(command2)
 
    # Primary wave direction
    grib2dump = 'DIRPW_extract_f'+str((tstep-1)*TINCR).zfill(3)+'.txt'
@@ -116,12 +119,15 @@ else:
 SITEID = os.environ.get('SITEID')
 CGNUMPLOT = os.environ.get('CGNUMPLOT')
 
+fieldmax = 'HTSGW_extract_fieldmax_TSTART'+str(TSTART)+'.txt'
+command = '$WGRIB2 '+DSET+' -s | grep "HTSGW" | $WGRIB2 -i '+DSET+' -max | cat > '+fieldmax
+os.system(command)
 temp=np.loadtxt(fieldmax, delimiter='=', usecols=[1])
 maxval=max(temp)
 
 plt.figure()
 # Read the extracted text file
-for tstep in range(1, (int(TDEF)+1)):
+for tstep in range(TSTART, (int(TEND)+1)):
    print('')
    print('Processing Time step: '+str(tstep))
 
@@ -259,6 +265,7 @@ for tstep in range(1, (int(TDEF)+1)):
    plt.clf()
 
 # Clean up text dump files
-os.system('rm HTSGW_extract*f???.txt')
-os.system('rm DIRPW_extract*f???.txt')
+for tstep in range(TSTART, (int(TEND)+1)):
+   os.system('rm HTSGW_extract_f'+str((tstep-1)*TINCR).zfill(3)+'.txt')
+   os.system('rm DIRPW_extract_f'+str((tstep-1)*TINCR).zfill(3)+'.txt')
 
