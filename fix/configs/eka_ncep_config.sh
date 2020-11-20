b86337a1e3d58f9608b8be9ff6267733141e6857
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-11-20 17:12:29 +0000

    Updated ESTOFS naming from estofs.glo to estofs

--- fix/configs/eka_ncep_config.sh
+++ fix/configs/eka_ncep_config.sh
@@ -1,14 +1,14 @@
 # NCEP Config file for global RTOFS and ESTOF init files
 
 # RTOFS Domain for ocean currents
-export RTOFSSECTOR="west_conus.west"
+export RTOFSSECTOR="west_conus"
 # RTOFSDOMAIN="LON LAT 0. NX NY EW-RESOLUTION NS-RESOLUTION"
 export RTOFSDOMAIN="233.23 37.90 0.0  134  178 0.029326 0.027027"
 export RTOFSNX="135"
 export RTOFSNY="179"
 
 # ESTOFS Domain for water level
-export ESTOFS_BASIN="estofs.glo"
+export ESTOFS_BASIN="estofs"
 export ESTOFS_REGION="conus.west"
 # ESFOTSDOMAIN="LON LAT 0. NX NY EW-RESOLUTION NS-RESOLUTION"
 export ESTOFSDOMAIN="233.23 37.90 0.0  134  178 0.029326 0.027027"
