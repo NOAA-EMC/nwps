96ab98c74aae0dd4af8e6da15f8bb152ebc7c367
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-11-20 17:10:44 +0000

    Updated ESTOFS naming from estofs.glo to estofs

--- fix/configs/mob_ncep_config.sh
+++ fix/configs/mob_ncep_config.sh
@@ -8,7 +8,7 @@ export RTOFSNX="145"
 export RTOFSNY="131"
 
 # ESTOFS Domain for water level
-export ESTOFS_BASIN="estofs.glo"
+export ESTOFS_BASIN="estofs"
 export ESTOFS_REGION="conus.east"
 # ESFOTSDOMAIN="LON LAT 0. NX NY EW-RESOLUTION NS-RESOLUTION"
 export ESTOFSDOMAIN="270.40 28.00  0.0 144 130 0.029326 0.027027"
