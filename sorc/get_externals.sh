d34672b38c6efb93497f65d8ef08a7345b0c5c49
awest-noaa <andre.vanderwesthuysen@noaa.gov>
2021-10-18 14:08:28 +0000

    Updated location of large fix files for WCOSS2

--- sorc/get_externals.sh
+++ sorc/get_externals.sh
@@ -13,19 +13,19 @@ if [ "${NWPSdir}" == "" ]
 fi
 
 echo 'Fetching externals...'
-scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/fix/bathy_db.tar ${NWPSdir}/fix/
+scp /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_nwps/v_1-3/fix/bathy_db.tar ${NWPSdir}/fix/
 if [ -d "${NWPSdir}/fix/bathy_db" ]; then rm -Rf "${NWPSdir}/fix/bathy_db"; fi
 mkdir -p ${NWPSdir}/fix/bathy_db/
 tar -C ${NWPSdir}/fix/bathy_db/ -xvf ${NWPSdir}/fix/bathy_db.tar
 rm ${NWPSdir}/fix/bathy_db.tar
-scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/fix/pdef_ncep_global ${NWPSdir}/fix/
-scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/ush/rtofs/datfiles/pdef_ncep_global.gz ${NWPSdir}/ush/rtofs/datfiles/
-scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/ush/rtofs/datfiles/pdef_ncep_reg1.gz ${NWPSdir}/ush/rtofs/datfiles/
-scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/ush/rtofs/datfiles/pdef_ncep_reg2.gz ${NWPSdir}/ush/rtofs/datfiles/
-scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/ush/rtofs/datfiles/pdef_ncep_reg3.gz ${NWPSdir}/ush/rtofs/datfiles/
-scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/ush/python/etc/default/rdat.tar ${NWPSdir}/ush/python/etc/default
+scp /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_nwps/v_1-3/fix/pdef_ncep_global ${NWPSdir}/fix/
+scp /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_nwps/v_1-3/ush/rtofs/datfiles/pdef_ncep_global.gz ${NWPSdir}/ush/rtofs/datfiles/
+scp /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_nwps/v_1-3/ush/rtofs/datfiles/pdef_ncep_reg1.gz ${NWPSdir}/ush/rtofs/datfiles/
+scp /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_nwps/v_1-3/ush/rtofs/datfiles/pdef_ncep_reg2.gz ${NWPSdir}/ush/rtofs/datfiles/
+scp /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_nwps/v_1-3/ush/rtofs/datfiles/pdef_ncep_reg3.gz ${NWPSdir}/ush/rtofs/datfiles/
+scp /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_nwps/v_1-3/ush/python/etc/default/rdat.tar ${NWPSdir}/ush/python/etc/default
 tar -C ${NWPSdir}/ush/python/etc/default/ -xvf ${NWPSdir}/ush/python/etc/default/rdat.tar
 rm ${NWPSdir}/ush/python/etc/default/rdat.tar
-scp /gpfs/hps3/emc/marine/noscrub/emc.wavepa/git/nwps-externals/v_1-3/lib/cartopy_shapefiles.tgz ${NWPSdir}/lib/cartopy_shapefiles.tgz
+scp /lfs/h2/emc/couple/noscrub/Andre.VanderWesthuysen/git/fv3gfs/fix/fix_nwps/v_1-3/lib/cartopy_shapefiles.tgz ${NWPSdir}/lib/cartopy_shapefiles.tgz
 tar -C ${NWPSdir}/lib/ -xvf ${NWPSdir}/lib/cartopy_shapefiles.tgz
 rm ${NWPSdir}/lib/cartopy_shapefiles.tgz
