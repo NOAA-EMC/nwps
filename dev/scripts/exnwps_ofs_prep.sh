8926064a8319a1e76b03234793bbb2a6699246d7
Ali Salimi <ali.salimi@clogin01.cactus.wcoss2.ncep.noaa.gov>
2026-03-30 19:40:38 +0000

    removing the commented rm -rf spool dir for estofs_ofs

--- dev/scripts/exnwps_ofs_prep.sh
+++ dev/scripts/exnwps_ofs_prep.sh
@@ -49,13 +49,11 @@ case ${OFSTYPE} in
         export CYCLE="${CYC}"
         ${USHnwps}/make_estofs.sh
         export err=$?; err_chk
-        #rm -rf ${RUNdir}/{*hourly,*hourly.spool}
     ;;
     estofs_cur)
         export CYCLE="${CYC}"
         ${USHnwps}/make_estofs_cur.sh
         export err=$?; err_chk
-        #rm -rf ${RUNdir}/{*hourly,*hourly.spool}
     ;;
     rtofs)
         export CYCLE="00"
